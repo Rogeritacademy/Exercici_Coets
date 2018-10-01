b8b4ef2a9db6d084351d9f2988dd0d4f98aced1a
Roger Masellas <roger@lamosca.com>
2018-10-01 13:11:02 +0200

    començo Fase 2

--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,20 @@
 /*jshint esversion: 6 */
 
 /*
-FASE 1:
-Volem fer un software de carreres de coets.
-Un coet està identificat per un codi de 8 caràcters i un número de propulsors.
-Realitza els següents passos:
-Creem dos coets amb els codis “32WESSDS” i “LDSFJA32”. El primer coet tindrà tres propulsors i el segon sis propulsors.
-Mostrar a pantalla el codi dels coets i el número de propulsors que té.
+FASE 2:
+Volem millorar el software perquè el propulsor tingui una potència màxima.
+Modifiquem en el main anterior:
+Creem dos coets amb els codis “32WESSDS” I “LDSFJA32”. El primer coet tindrà tres propulsors (potència màxima: 10,30,80)
+i el segon sis propulsors (potència màxima: 30,40,50,50,30,10).
+Mostrar a pantalla el codi dels coets, el número de propulsors que té i la potència màxima de cada propulsor.
+
+Output:
+32WESSDS: 10,30,80
+LDSFJA32: 30,40,50,50,30,10
 */
+
+let rocket1MaxPower = [10,30,80];
+let rocket2MaxPower = [30,40,50,50,30,10];
 
 const rocket1 = new Rocket("Rocket1", "32WESSDS", "3");
 const rocket2 = new Rocket("Rocket2", "LDSFJA32", "6");
@@ -15,7 +22,7 @@ const rocket2 = new Rocket("Rocket2", "LDSFJA32", "6");
 function showData(rocket) {
   var newDiv = document.createElement("div");
   newDiv.id = rocket.rocketName;
-  newDiv.innerHTML = '<p>' + rocket.rocketName + '<br>' + rocket.serialNum + '<br>' + rocket.propellersNum + '</p>';
+  newDiv.innerHTML = '<p>' + rocket.rocketName + '<br>Serial: ' + rocket.serialNum + '<br>Propellers: ' + rocket.propellersNum + '</p>';
   document.getElementById('main').appendChild(newDiv);
 }
 
--- js/model.js
+++ js/model.js
@@ -1,9 +1,36 @@
 /*jshint esversion: 6 */
 
 class Rocket {
-  constructor(rocketName, serialNum, propellersNum) {
+  constructor(rocketName, serialNum, propellersNum, rocketMaxPower) {
     this.rocketName = rocketName;
     this.serialNum = serialNum;
     this.propellersNum = propellersNum;
+    this.rocketMaxPower = rocketMaxPower;
   }
 }
+
+
+/*** prova ***/
+
+class Foo {
+  constructor (...args) {
+    for (let i = 0; i < args.length; i++) {
+      this[i] = args[i];
+    }
+  }
+
+  [Symbol.iterator]() {
+    return Object
+      .keys(this)
+      .map(key => this[key])
+      .values();
+  }
+}
+
+const foo = new Foo('a', 'b', 'c');
+
+for (let item of foo) {
+  console.log(item); // prints out a, b, c
+}
+
+console.log(foo[1]); // prints out b
